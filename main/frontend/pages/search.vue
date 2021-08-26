b34d02c45041e0e3c73bf9eabe5be315be3cc6ed
rauan <rauanamigo@hotmail.com>
2021-08-25 23:00:23 -0300

    created search page

--- main/frontend/pages/search.vue
+++ main/frontend/pages/search.vue
@@ -1,10 +1,27 @@
 <template>
-  <div></div>
+  <div>
+    <div>Sua busca por "{{ search }}" retornou 0 resultados</div>
+    <div>
+      <vue-table
+        :fields="['nome', 'interações', 'Aprovações', 'Rejeições']"
+        :data="[]"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
+import VueTable from "vuetable-2";
 export default {
   name: "Search",
+  components: {
+    VueTable,
+  },
+  computed: {
+    search() {
+      return this.$route.query.search;
+    },
+  },
 };
 </script>
 
---
+++ main/frontend/plugins/VueTable.js
@@ -0,0 +1,4 @@
+import Vue from "vue";
+import VueTable from "vuetable-2";
+
+Vue.use(VueTable);
