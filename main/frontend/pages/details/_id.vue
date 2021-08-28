a96b1a2f8b5af319742f31c3105300ac186ce97d
rauan <rauanamigo@hotmail.com>
2021-08-28 01:56:57 -0300

    setup pie chart

--- main/frontend/pages/details/_id.vue
+++ main/frontend/pages/details/_id.vue
@@ -36,30 +36,7 @@ export default {
   data() {
     return {
       subject: null,
-      comments: [
-        {
-          id: 0,
-          author: {
-            name: "teste",
-            src: "",
-          },
-          content:
-            "Lorem ipsum facilisis sociosqu vestibulum posuere sed metus mi inceptos mauris, aptent lorem rutrum ante volutpat fusce condimentum porttitor tellus, integer vitae consequat primis suspendisse tellus sagittis curabitur vel. curabitur morbi lacinia augue dui dolor, fusce pharetra hac. vitae adipiscing quis egestas blandit class nostra sociosqu inceptos, venenatis habitasse consequat laoreet lorem viverra nec cursus scelerisque, turpis sollicitudin aenean cursus dapibus aenean erat. sit urna ultricies ut leo nibh sem justo hendrerit mi libero interdum convallis, luctus erat nisl cras eu augue quisque pretium donec tellus. lacus integer ipsum torquent metus vulputate orci curae ut, hendrerit nostra auctor aliquam scelerisque volutpat vestibulum eleifend odio, quisque molestie donec turpis vulputate proin morbi.",
-          is_positive: false,
-          date: "2021-10-10",
-        },
-        {
-          id: 1,
-          author: {
-            name: "teste",
-            src: "",
-          },
-          content:
-            "Lorem ipsum facilisis sociosqu vestibulum posuere sed metus mi inceptos mauris, aptent lorem rutrum ante volutpat fusce condimentum porttitor tellus, integer vitae consequat primis suspendisse tellus sagittis curabitur vel. curabitur morbi lacinia augue dui dolor, fusce pharetra hac. vitae adipiscing quis egestas blandit class nostra sociosqu inceptos, venenatis habitasse consequat laoreet lorem viverra nec cursus scelerisque, turpis sollicitudin aenean cursus dapibus aenean erat. sit urna ultricies ut leo nibh sem justo hendrerit mi libero interdum convallis, luctus erat nisl cras eu augue quisque pretium donec tellus. lacus integer ipsum torquent metus vulputate orci curae ut, hendrerit nostra auctor aliquam scelerisque volutpat vestibulum eleifend odio, quisque molestie donec turpis vulputate proin morbi.",
-          is_positive: false,
-          date: "2021-10-10",
-        },
-      ],
+      comments: [],
     };
   },
   computed: {
@@ -75,20 +52,26 @@ export default {
         legend: {
           orient: "vertical",
           left: "left",
+          data: ["Aprovação", "Reprovação"],
         },
         series: [
           {
-            name: "访问来源",
+            name: "Total",
             type: "pie",
             radius: "50%",
+            label: {
+              position: "inner",
+              fontSize: 14,
+              formatter: "{c}",
+            },
             data: [
               {
-                value: 1048,
+                value: this.subject.timeline_set?.approval_percentage || 0,
                 name: "Aprovação",
                 itemStyle: { color: "#4ADE80" },
               },
               {
-                value: 735,
+                value: this.subject.timeline_set?.disapproval_percentage || 0,
                 name: "Reprovação",
                 itemStyle: { color: "#F87171" },
               },
@@ -104,6 +87,12 @@ export default {
         ],
       };
     },
+    approvalPercentage() {
+      return this.subject.timeline_set?.approval_percentage || 0;
+    },
+    disapprovalPercentage() {
+      return this.subject.timeline_set?.disapproval_percentage || 0;
+    },
   },
 };
 </script>
@@ -114,7 +103,7 @@ export default {
 }
 
 .subject__chart {
-  @apply w-full h-64 bg-tertiary mt-2;
+  @apply w-full flex justify-center items-center h-64 bg-tertiary mt-2 p-4;
 }
 
 @screen md {
