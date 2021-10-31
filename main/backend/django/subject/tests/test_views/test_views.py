c25b189af280299d50bf49a91da26d0a764156e5
Julian Pedro F. Braga <julian.pedro@hotmail.com>
2021-10-31 02:13:35 -0400

    Add more subject case test

--- main/backend/django/subject/tests/test_views/test_views.py
+++ main/backend/django/subject/tests/test_views/test_views.py
@@ -1,3 +1,4 @@
+import random
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
@@ -31,4 +32,26 @@ class SubjectTestCase(APITestCase):
         self.assertEqual(response_subject_object.get('timeline_set', {}).get('id'),
                          str(self.subject_saved.timeline_set.first().id))
 
-    # TODO: Get subject with search and check if create new subject when not found
+    def test_if_get_searched_subject(self):
+        """ Try get subject searched in request API. """
+        subject_hashtag = self.subject_saved.hashtag
+        response = self.client.get(f'{self.subject_url}?search={subject_hashtag}')
+        response_object = response.json()
+        response_subjects_count = response_object.get('count')
+        response_subject_object = response_object.get('results')[0]
+        self.assertEqual(Subject.objects.count(), response_subjects_count)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response_subject_object.get('id'), str(self.subject_saved.id))
+        self.assertEqual(response_subject_object.get('registration_date'),
+                         self.subject_saved.registration_date.strftime('%Y-%m-%dT%H:%M:%S.%fZ'))
+        self.assertEqual(response_subject_object.get('timeline_set', {}).get('id'),
+                         str(self.subject_saved.timeline_set.first().id))
+
+    def test_not_found_subject(self):
+        """ Try get not found subject. """
+        subject_hashtag = f'Test{random.randint(0, 100)}'
+        response = self.client.get(f'{self.subject_url}?search={subject_hashtag}')
+        response_object = response.json()
+        response_subjects_count = response_object.get('count')
+        self.assertEqual(0, response_subjects_count)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
